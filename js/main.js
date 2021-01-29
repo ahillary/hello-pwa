ed88f1e3313041f461a02558b9d9c4678ec93a00
Hillary Jenkins <hello.hillary7@gmail.com>
2021-01-29 13:08:39 -0800

    merged correctly

--- js/main.js
+++ js/main.js
@@ -2,13 +2,6 @@ window.onload = () => {
   'use strict';
 
   if ('serviceWorker' in navigator) {
-<<<<<<< HEAD
     navigator.serviceWorker.register('./sw.js');
   }
 };
-=======
-    navigator.serviceWorker
-             .register('./sw.js');
-  }
-}
->>>>>>> d4a153a94cae5cb891ca9cadc536b9a4811ea451
